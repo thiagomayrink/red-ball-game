3baf1105571b631ecf44e822f364a31a05c610a4
thiagomayrink <thiagoscmpaiva@gmail.com>
2021-08-20 22:59:17 -0300

    feature: add scoreboard

--- handlers/scoreHandler.js
+++ handlers/scoreHandler.js
@@ -1,8 +1,14 @@
 function incrementScore() {
   playerScore++;
-  console.log(playerScore);
 }
 
 function resetScore() {
   playerScore = 0;
+}
+
+function drawScore() {
+  context.font = "48px Arial";
+  context.fillStyle = "#000000";
+  const middleX = screenWidth / 2;
+  context.fillText(`Score: ${playerScore}`, middleX - 80, 52);
 }
